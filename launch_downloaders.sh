ada54498603456e4af7606aa6c040c8697ff91eb
Alex Kassil <alexkassil@sbcglobal.net>
2019-01-12 01:52:34 -0800

    Change it to individual seasons

--- launch_downloaders.sh
+++ launch_downloaders.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # number of parallel tasks to run downloading photos
-N=16
+N=4
 for i in $(seq 1 $N)
 do
-	python2 download_photos.py $i $N &
+	python2 download_photos.py $i $N autumn &
+	python2 download_photos.py $i $N winter &
+	python2 download_photos.py $i $N summer &
+	python2 download_photos.py $i $N spring &
 done
 
